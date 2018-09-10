dcb7bbed9b25f3179f439cabf99e27beb21e2f68
Aleksey <lesha.leshka@inbox.ru>
2018-09-10 18:18:47 +0300

    Shared state 101

--- public/js/app.js
+++ public/js/app.js
@@ -1,181 +1,23 @@
-class Errors {
-    /**
-     * Create a new Errors instance.
-     */
-    constructor() {
-        this.errors = {};
+let store = {
+    user: {
+        name: 'John Doe'
     }
-
-    /**
-     * Determine if an errors exists for the given field.
-     * 
-     * @param {string} field 
-     */
-    has(field) {
-        return this.errors.hasOwnProperty(field);
-    }
-    
-    /**
-     * Ddetermine if we have any errors.
-     */
-    any() {
-        return Object.keys(this.errors).length > 0;
-    }
-
-    /**
-     * Retrieve the error message for a field.
-     * 
-     * @param {string} field 
-     */
-    get(field) {
-        if (this.errors[field]) {
-            return this.errors[field][0];
-        }
-    }
-
-    /**
-     * Record the new errors.
-     * 
-     * @param {object} errors 
-     */
-    record(errors) {
-        this.errors = errors.errors;
-    }
-
-    /**
-     * Clear one all error fields.
-     * 
-     * @param {string|null} field 
-     */
-    clear(field) {
-        if (field) {
-            delete this.errors[field];
-
-            return;
-        } 
-
-        this.errors = {};
-    }
-}
-
-class Form {
-    /**
-     * Create a new form instance.
-     * 
-     * @param {object} data 
-     */
-    constructor(data) {
-        this.originalData = data;
-
-        for(let field in data) {
-            this[field] = data[field];
-        }
-
-        this.errors = new Errors();
-    }
-
-    /**
-     * Fetch all relevant data for the form.
-     */
-    data() {
-        let data = {};
-
-        for (let property in this.originalData) {
-            data[property] = this[property];
-        }
-
-        return data;
-    }
-
-    /**
-     * Reset the form fields.
-     */
-    reset() {
-        for(let field in this.originalData) {
-            this[field] = '';
-        }
-        
-        this.errors.clear();
-    }
-
-    /**
-     * Send a POST request to the given URL.
-     * @param {string} url 
-     */
-    post(url) {
-        return this.submit('post', url);
-    }
-
-    /**
-     * Sent a DELETE request to the given URL.
-     * @param {string} url 
-     */
-    delete(url) {
-        return this.submit('delete', url);
-    }
-
-    /**
-     * Submit the form.
-     * 
-     * @param {string} requestType 
-     * @param {string} url 
-     */
-    submit(requestType, url) {
-        return new Promise((resolve, reject) => {
-            axios[requestType](url, this.data())
-                .then(response => {
-                    this.onSuccess(response.data);
-
-                    resolve(response.data);
-                })
-                .catch(error => {
-                    this.onFail(error.response.data);
-
-                    reject(error.response.data);
-                });
-
-        });
-    }
-
-    /**
-     * Handle a successful form submissions.
-     * 
-     * @param {object} data
-     */
-    onSuccess(data) {
-        alert(data.message);
-
-        this.reset();
-    }
-
-    /**
-     * Handle a failed form submission.
-     * 
-     * @param {object} errors
-     */
-    onFail(errors) {
-        this.errors.record(errors);
-
-        this.loading = false;
-    }
-}
+};
 
 new Vue({
-    el: '#app',
+    el: '#one',
 
     data: {
-        form: new Form({
-            name: '',
-            description: '',
-            loading: false
-        })
-    },
+        foo: 'bar',
+        shared: store
+    }
+});
 
-    methods: {
-        onSubmit() {
-            this.form.loading = true;
+new Vue({
+    el: '#two',
 
-            this.form.post('/projects');
-        }
+    data: {
+        foo: 'other',
+        shared: store
     }
 });
