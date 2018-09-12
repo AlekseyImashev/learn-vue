d2899616ee39483fd26c4f82a5c76e75560c58cc
Aleksey <lesha.leshka@inbox.ru>
2018-09-12 15:50:49 +0300

    SPAs and the backend

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
         <link rel="stylesheet" href="/css/app.css">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>My App</title>
     </head>
